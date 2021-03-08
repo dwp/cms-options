bcdf16a1565edcdd0a104e9031a43c43b8c8c93c
RossDBrown <ross.david.brown@gmail.com>
2021-03-08 10:17:04 +0000

    Created sprint 4

--- app/routes.js
+++ app/routes.js
@@ -7,10 +7,12 @@ const router = express.Router()
 const version1 = require('./routes/version1');;
 const govuk1 = require('./routes/govuk1');;
 const cycle2 = require('./routes/cycle2');;
+const cycle4 = require('./routes/cycle4');;
 
 //copy the router use and update the sprint version
 router.use(version1);
 router.use(govuk1);
 router.use(cycle2);
+router.use(cycle4);
 
 module.exports = router
---
+++ app/routes/cycle4.js
@@ -0,0 +1,73 @@
+
+const express = require('express');
+const router = express.Router()
+
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+
+// Choice
+router.post('/cycle4/choice', function(req, res) {
+  if (req.body['choice'] === 'private') {
+    res.redirect('private');
+  } else {
+    res.redirect('apply');
+  }
+});
+
+
+// Apply
+router.post('/cycle4/apply', function(req, res) {
+  if (req.body['apply-type'] === 'online') {
+    res.redirect('intro');
+  } else {
+    res.redirect('phone');
+  }
+});
+
+// Live in UK
+router.post('/cycle4/live-in-uk', function(req, res) {
+  if (req.body['live-uk'] === 'yes') {
+    res.redirect('court-order');
+  } else {
+    res.redirect('not-eligible-uk');
+  }
+});
+
+// Court Order
+router.post('/cycle4/court-order', function(req, res) {
+  if (req.body['maintenance-liability'] === 'yes') {
+    res.redirect('not-eligible-court');
+  } else {
+    res.redirect('eligible');
+  }
+});
+
+
+// Choice NOURN
+router.post('/cycle4/no-urn/choice', function(req, res) {
+  if (req.body['choice'] === 'private') {
+    res.redirect('private');
+  } else {
+    res.redirect('apply');
+  }
+});
+
+
+// Apply NOURN
+router.post('/cycle4/no-urn/apply', function(req, res) {
+  if (req.body['apply-type'] === 'online') {
+    res.redirect('intro');
+  } else {
+    res.redirect('phone');
+  }
+});
+
+
+
+module.exports = router;
