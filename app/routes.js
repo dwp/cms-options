5af4f175a4bcd5844dfbf0988eec4a625d7b8bc8
RossDBrown <ross.david.brown@gmail.com>
2021-03-10 10:44:40 +0000

    Paying and Receiving Parent content
    Branching path

--- app/routes.js
+++ app/routes.js
@@ -8,11 +8,15 @@ const version1 = require('./routes/version1');;
 const govuk1 = require('./routes/govuk1');;
 const cycle2 = require('./routes/cycle2');;
 const cycle4 = require('./routes/cycle4');;
+const cycle4pp = require('./routes/cycle4pp');;
+const cycle4rp = require('./routes/cycle4rp');;
 
 //copy the router use and update the sprint version
 router.use(version1);
 router.use(govuk1);
 router.use(cycle2);
 router.use(cycle4);
+router.use(cycle4pp);
+router.use(cycle4rp);
 
 module.exports = router
