6a4346a8666e658c8460a1b3be162d5f9933d826
RossDBrown <ross.david.brown@gmail.com>
2021-05-21 11:37:49 +0100

    Created new iteration for WIP this cycle

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,7 @@ const prototype1iteration2 = require('./routes/prototype1iteration2');;
 const prototype2iteration1 = require('./routes/prototype2iteration1');;
 const prototype3iteration1 = require('./routes/prototype3iteration1');;
 const prototype4iteration1 = require('./routes/prototype4iteration1');;
+const prototype4iteration2 = require('./routes/prototype4iteration2');;
 const telephonytool = require('./routes/telephonytool');;
 
 //copy the router use and update the sprint version
@@ -17,6 +18,7 @@ router.use(prototype1iteration2);
 router.use(prototype2iteration1);
 router.use(prototype3iteration1);
 router.use(prototype4iteration1);
+router.use(prototype4iteration2);
 router.use(telephonytool);
 
 module.exports = router
