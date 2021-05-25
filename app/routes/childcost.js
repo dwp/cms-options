236ee9010125fa5708d1d109ab163ae6df7ee1d6
Anthony E <anthony.elstob@opencastsoftware.com>
2021-05-25 16:34:56 +0100

    First few pages for child cost calculator
    First few pages for child cost calculator

---
+++ app/routes/childcost.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const router = express.Router()
+
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+
+
+//childcost
+
+// how many children logic
+router.post('/childcost/howmanychildren', function(req, res) {
+  if (req.body['how-many-children'] === '1') {
+    res.redirect('childtype1');
+  } else {
+    res.redirect('childtypemultiple');
+  }
+});
+
+
+
+// Routes end
+
+module.exports = router;
