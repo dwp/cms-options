3f9b12b1a8f0b0d33a53361e79139a6029f61a60
Craig Lang <craig.lang@opencastsoftware.com>
2022-03-31 14:36:39 +0100

    Updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get Help Arranging Child Maintenance',
+  serviceName: 'Get help arranging child maintenance',
 
   // Default port that prototype runs on
   port: '3000',
