0fd6bdc7a52f8e8c57199605bc721d85744f8886
Craig Lang <craig.lang@opencastsoftware.com>
2022-04-05 13:01:15 +0100

    Changes to information journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get help arranging child maintenance',
+  serviceName: 'Get Help Arranging Child Maintenance',
 
   // Default port that prototype runs on
   port: '3000',
